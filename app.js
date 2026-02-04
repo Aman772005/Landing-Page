1aed68ca8b31ccfa8e0ee033b3e1c794243450a4
Aman772005 <amansinghtomar772005@gmail.com>
2026-02-04 13:30:19 +0530

    change

--- app.js
+++ app.js
@@ -7,3 +7,20 @@ function hidemenu(){
 function openmenu(){
     a.style.right = "0"
 }
+
+import emailjs from 'emailjs-com';
+
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function(e){
+  e.preventDefault();
+
+  emailjs.sendForm(
+    "service_id",
+    "template_id",
+    this,
+    "public_key"
+  ).then(() => {
+    alert("Sent");
+  });
+});

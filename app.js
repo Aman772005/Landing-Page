d13da452dc468c9178679ba9e08c6bdc6fc9bd54
Aman772005 <amansinghtomar772005@gmail.com>
2026-02-04 13:50:58 +0530

    completed
    Using HTML,CSS,JavaScript .I have made this landing page responsive and attractive

--- app.js
+++ app.js
@@ -8,19 +8,3 @@ function openmenu(){
     a.style.right = "0"
 }
 
-import emailjs from 'emailjs-com';
-
-const form = document.getElementById("contactForm");
-
-form.addEventListener("submit", function(e){
-  e.preventDefault();
-
-  emailjs.sendForm(
-    "service_id",
-    "template_id",
-    this,
-    "public_key"
-  ).then(() => {
-    alert("Sent");
-  });
-});
